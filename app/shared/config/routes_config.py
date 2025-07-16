adac2314969c9955cbfa111835926033f56cc969
Aga Bachleda <agnieszka.bachleda@justice.gov.uk>
2025-07-16 10:53:06 +0100

    added acronyms project to the app (#82)

--- app/shared/config/routes_config.py
+++ app/shared/config/routes_config.py
@@ -5,6 +5,7 @@ from app.projects.repository_standards.routes.deprecated import (
     repository_standards_deprecated,
 )
 from app.projects.repository_standards.routes.main import repository_standards_main
+from app.projects.acronyms.routes.main import acronyms_main
 from app.shared.routes.auth import auth_route
 from app.shared.routes.main import main
 from app.shared.routes.robots import robot_route
@@ -22,3 +23,7 @@ def configure_routes(app: Flask) -> None:
         repository_standards_api, url_prefix="/repository-standards/api"
     )
     app.register_blueprint(repository_standards_deprecated, url_prefix="/")
+
+    app.register_blueprint(
+        acronyms_main, url_prefix="/acronyms/"
+    )
