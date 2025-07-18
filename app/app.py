63eaa07878d987f8d7da5cf13b683805b081295d
abachleda-baca <agnieszka.bachleda@justice.gov.uk>
2025-07-18 16:02:28 +0100

    enable migration on the app start

--- app/app.py
+++ app/app.py
@@ -1,9 +1,11 @@
 import logging
 
 from flask import Flask
+from flask_migrate import upgrade
 
 from app.projects.repository_standards.config.stub_data_config import create_stub_data
-from app.projects.repository_standards.db_models import db
+from app.shared.database import db
+from app.shared.database import db_migration
 from app.shared.config.app_config import app_config
 from app.shared.config.cors_config import configure_cors
 from app.shared.config.error_handlers_config import configure_error_handlers
@@ -27,8 +29,10 @@ def create_app(is_rate_limit_enabled=True) -> Flask:
 
     app.config["SQLALCHEMY_DATABASE_URI"] = app_config.postgres.sql_alchemy_database_url
     db.init_app(app)
+    db_migration.init_app(app, db)
+
     with app.app_context():
-        db.create_all()
+       upgrade()
 
     configure_routes(app)
     configure_error_handlers(app)
